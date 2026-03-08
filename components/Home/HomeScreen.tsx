1f4628c5d5b42e32140a1dceb8bd5f482d3d993e
المبرمج شو <157006538+almoparmgshoGit@users.noreply.github.com>
2026-03-08 06:54:20 +0200

    Update HomeScreen.tsx

--- components/Home/HomeScreen.tsx
+++ components/Home/HomeScreen.tsx
@@ -2,8 +2,7 @@ import { auth } from "@/config/firebase";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { onAuthStateChanged } from "firebase/auth";
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, Text, View } from "react-native";
 import Header from '../Custom/Header';
 
 const HomeScreen = () => {
@@ -38,8 +37,11 @@ const HomeScreen = () => {
 
         return () => unsubscribe();
     }, []);
+
+
+
     return (
-        <View className="">
+        <View className="" style={{ flex: 1 }}>
             <Header title="الرئيسية" />
 
             <View>
@@ -57,30 +59,44 @@ const HomeScreen = () => {
                 </View>
             </View>
 
-            <View className="">
+            <View className="flex-1" style={{ flex: 1 }} >
                 <Text className="text-2xl font-bold text-text-muted ml-6 mt-6 ">اخر الخجوزات</Text>
-                <ScrollView>
-                    <View className="w-full px-4 items-center mt-7">
+                <ScrollView contentContainerStyle={{ flexGrow: 4 }}>
+                    <View className="w-full px-4 items-center mt-7 pb-10">
                         <View style={{ direction: 'rtl' }} className="w-full  px-4 py-2 text-text-main mt-4 bg-primary border border-border rounded-lg">
-                            <Text>امس</Text>
+                            <Text className="text-white  font-bold text-xl">امس</Text>
+                            <Text className="text-white  font-bold text-xl">بتاريخ : 2022-01-01</Text>
+                            <Text className="text-white  font-bold text-xl">12:00 AM</Text>
+                            <Text className="text-white  font-bold text-xl">عيادة الدكتور احمد</Text>
+                            <Text className="text-white  bg-green-500 rounded-full px-2 py-1 text-center mt-4 w-20 font-bold text-sm">منتهي</Text>
                         </View>
                         <View style={{ direction: 'rtl' }} className="w-full  px-4 py-2 text-text-main mt-4 bg-primary border border-border rounded-lg">
-                            <Text>امس</Text>
+                            <Text className="text-white  font-bold text-xl">امس</Text>
+                            <Text className="text-white  font-bold text-xl">بتاريخ : 2022-01-01</Text>
+                            <Text className="text-white  font-bold text-xl">10:00 AM</Text>
+                            <Text className="text-white  font-bold text-xl">عيادة الدكتور احمد</Text>
+                            <Text className="text-white  bg-green-500 rounded-full px-2 py-1 text-center mt-4 w-20 font-bold text-sm">منتهي</Text>
                         </View>
                         <View style={{ direction: 'rtl' }} className="w-full  px-4 py-2 text-text-main mt-4 bg-primary border border-border rounded-lg">
-                            <Text>امس</Text>
+                            <Text className="text-white  font-bold text-xl">اليوم</Text>
+                            <Text className="text-white  font-bold text-xl">بتاريخ : 2022-01-01</Text>
+                            <Text className="text-white  font-bold text-xl">09:00 AM</Text>
+                            <Text className="text-white  font-bold text-xl">عيادة الدكتور احمد</Text>
+                            <Text className="text-white  bg-red-600 rounded-full px-2 py-1 text-center mt-4 w-20 font-bold text-sm">ملغي</Text>
                         </View>
                         <View style={{ direction: 'rtl' }} className="w-full  px-4 py-2 text-text-main mt-4 bg-primary border border-border rounded-lg">
-                            <Text>امس</Text>
+                            <Text className="text-white  font-bold text-xl">من يومين</Text>
+                            <Text className="text-white  font-bold text-xl">بتاريخ : 2022-01-01</Text>
+                            <Text className="text-white  font-bold text-xl">04:00 AM</Text>
+                            <Text className="text-white  font-bold text-xl">عيادة الدكتور احمد</Text>
+                            <Text className="text-white  bg-yellow-600 rounded-full px-2 py-1 text-center mt-4 w-20 font-bold text-sm">تاخرت</Text>
                         </View>
                         <View style={{ direction: 'rtl' }} className="w-full  px-4 py-2 text-text-main mt-4 bg-primary border border-border rounded-lg">
-                            <Text>امس</Text>
-                        </View>
-                        <View style={{ direction: 'rtl' }} className="w-full  px-4 py-2 text-text-main mt-4 bg-primary border border-border rounded-lg">
-                            <Text>امس</Text>
-                        </View>
-                        <View style={{ direction: 'rtl' }} className="w-full  px-4 py-2 text-text-main mt-4 bg-primary border border-border rounded-lg">
-                            <Text>امس</Text>
+                            <Text className="text-white  font-bold text-xl">امس</Text>
+                            <Text className="text-white  font-bold text-xl">بتاريخ : 2022-01-02</Text>
+                            <Text className="text-white  font-bold text-xl">05:00 AM</Text>
+                            <Text className="text-white  font-bold text-xl">عيادة الدكتور احمد</Text>
+                            <Text className="text-white  bg-red-500 rounded-full px-2 py-1 text-center mt-4 w-20 font-bold text-sm">منتهي</Text>
                         </View>
                     </View>
 
